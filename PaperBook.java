017dd55b5e607744dde180016a05c348dc11c081
Amr Safwat <amrsafwat23@hotmail,.com>
2025-07-08 20:35:50 +0300

    FIle Modification: created new paperbook class inherited from books

---
+++ PaperBook.java
@@ -0,0 +1,25 @@
+public class PaperBook extends Book implements Shippable {
+    private int quantity;
+
+    public PaperBook(String isbn, String title, int publictionYear, double price, int quantity) {
+        super(isbn, title, publictionYear, price);
+        this.quantity = quantity;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean isOutdated(int currentYear, int outdatedThreshold) {
+        int age = currentYear - getYearPublished();
+        return age > outdatedThreshold;
+    }
+
+    @Override
+    public void ship(String address) {
+        System.out.println("Paper book '" + getTitle() + "' (ISBN: " + getIsbn() + ") is being shipped to: " + address);
+    }
+}
